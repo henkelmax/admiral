f46f5fd4d89805b775167e4e712c25d3a5fad201
Max Henkel <max@maxhenkel.de>
2023-08-31 17:52:03 +0200

    Add javadocs for permissionmanager

--- core/src/main/java/de/maxhenkel/admiral/permissions/PermissionManager.java
+++ core/src/main/java/de/maxhenkel/admiral/permissions/PermissionManager.java
@@ -1,7 +1,24 @@
 package de.maxhenkel.admiral.permissions;
 
+import de.maxhenkel.admiral.annotations.RequiresPermission;
+import de.maxhenkel.admiral.Admiral;
+
+/**
+ * A permission manager that handles permissions defined with {@link RequiresPermission}.
+ * <br/>
+ * Must be registered with {@link Admiral.Builder#setPermissionManager(PermissionManager)}.
+ *
+ * @param <S> the command source
+ */
 public interface PermissionManager<S> {
 
+    /**
+     * Checks if the source has the permission.
+     *
+     * @param source     the command source
+     * @param permission the permission node
+     * @return true if the source has the permission
+     */
     boolean hasPermission(S source, String permission);
 
 }
