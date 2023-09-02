0d7528a6a7df4126e8928d92eb4e5ea8d7a4a091
Max Henkel <max@maxhenkel.de>
2023-09-02 12:35:40 +0200

    Add summonable entity reference argument type

--- fabric/src/main/java/de/maxhenkel/admiral/impl/MinecraftArgumentTypes.java
+++ fabric/src/main/java/de/maxhenkel/admiral/impl/MinecraftArgumentTypes.java
@@ -92,10 +92,11 @@ public class MinecraftArgumentTypes {
                 (RawArgumentTypeConverter) (context, name, value) -> value == null ? null : ObjectiveArgument.getObjective(context, name)
         );
 
-        registerReference(argumentRegistry, EntityReference.class, () -> Registries.ENTITY_TYPE, (ctx, name) -> new EntityReference(ResourceArgument.getEntityType(ctx, name)));
         registerReference(argumentRegistry, AttributeReference.class, () -> Registries.ATTRIBUTE, (ctx, name) -> new AttributeReference(ResourceArgument.getAttribute(ctx, name)));
         registerReference(argumentRegistry, ConfiguredFeatureReference.class, () -> Registries.CONFIGURED_FEATURE, (ctx, name) -> new ConfiguredFeatureReference(ResourceArgument.getConfiguredFeature(ctx, name)));
         registerReference(argumentRegistry, StructureReference.class, () -> Registries.STRUCTURE, (ctx, name) -> new StructureReference(ResourceArgument.getStructure(ctx, name)));
+        registerReference(argumentRegistry, EntityReference.class, () -> Registries.ENTITY_TYPE, (ctx, name) -> new EntityReference(ResourceArgument.getEntityType(ctx, name)));
+        registerReference(argumentRegistry, SummonableEntityReference.class, () -> Registries.ENTITY_TYPE, (ctx, name) -> new SummonableEntityReference(ResourceArgument.getSummonableEntityType(ctx, name)));
     }
 
     private static <T extends ReferenceBase<R>, R> void registerReference(ArgumentTypeRegistryImpl argumentRegistry, Class<T> clazz, Supplier<ResourceKey<Registry<R>>> registrySupplier, CommandBiFunction<CommandContext<CommandSourceStack>, String, T> referenceSupplier) {
