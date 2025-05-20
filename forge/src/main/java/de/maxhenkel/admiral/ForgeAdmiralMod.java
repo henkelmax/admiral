3c2a3ab38e94b84d21b041b1896a9889c878fa09
Max Henkel <max@maxhenkel.de>
2025-05-20 19:26:34 +0200

    Remove everything related to Minecraft

--- forge/src/main/java/de/maxhenkel/admiral/ForgeAdmiralMod.java
+++
@@ -1,7 +0,0 @@
-package de.maxhenkel.admiral;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod("admiral")
-public class ForgeAdmiralMod {
-}
--- neoforge/src/main/java/de/maxhenkel/admiral/NeoForgeAdmiralMod.java
+++
@@ -1,7 +0,0 @@
-package de.maxhenkel.admiral;
-
-import net.neoforged.fml.common.Mod;
-
-@Mod("admiral")
-public class NeoForgeAdmiralMod {
-}
