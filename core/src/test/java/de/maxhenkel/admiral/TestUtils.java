9afdd656f8a093154e8c7489cee0e503f2c44046
Max Henkel <max@maxhenkel.de>
2025-10-05 14:25:42 +0200

    Fix unit tests failing

--- core/src/test/java/de/maxhenkel/admiral/TestUtils.java
+++ core/src/test/java/de/maxhenkel/admiral/TestUtils.java
@@ -2,6 +2,7 @@ package de.maxhenkel.admiral;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import de.maxhenkel.admiral.impl.PermissionLevelManager;
 import de.maxhenkel.admiral.permissions.PermissionManager;
 
 import javax.annotation.Nullable;
@@ -22,6 +23,7 @@ public class TestUtils {
 
     public static void executeCommand(String command, Class<?> commandClass, int permissionLevel, @Nullable PermissionManager<Source> permissionManager) throws CommandSyntaxException {
         CommandDispatcher<Source> dispatcher = new CommandDispatcher<>();
+        PermissionLevelManager.setPermissionLevelAccessor((source, level) -> ((Source) source).hasPermission(level));
         Admiral.builder(dispatcher).addCommandClasses(commandClass).setPermissionManager(permissionManager).build();
         dispatcher.execute(command, new Source(permissionLevel));
     }
